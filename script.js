bf140e04e16308a6c5e2642a0bd00f021c9b5155
Tanuja Sinha <136427285+tanujasinha@users.noreply.github.com>
2023-08-24 23:05:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// Get the navigation icon and navigation tab elements
 const navIcon = document.getElementById('nav-icon');
 const navtab = document.getElementById('navtab');
+// Get all the sections and navbar links
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.navtab a');
 
+// Toggle navigation tab visibility on icon click
 navIcon.addEventListener('click', () => {
   navtab.classList.toggle('active');
 });
 
-// Get all the sections and navbar links
-const sections = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll('.navtab a');
+// Function to hide dropdown
+function hideDropdown() {
+  navtab.classList.remove('active');
+}
+
+// Add event listeners to hide dropdown when a link is clicked
+navLinks.forEach(link => {
+  link.addEventListener('click', hideDropdown);
+});
 
 // Function to determine which section is currently visible
 function getActiveSection() {
@@ -46,3 +57,24 @@ window.addEventListener('scroll', updateActiveLink);
 
 // Initial update on page load
 updateActiveLink();
+// Define the text for typing animation
+const typingText = "Explore my journey through technology, creativity, and innovation.";
+
+// Function to simulate typing animation
+function typeWriter(text, element, delay) {
+  let charIndex = 0;
+  const typingInterval = setInterval(() => {
+    if (charIndex < text.length) {
+      element.textContent += text.charAt(charIndex);
+      charIndex++;
+    } else {
+      clearInterval(typingInterval);
+    }
+  }, delay);
+}
+
+// Call the typing animation function on page load
+window.addEventListener('load', () => {
+  const typingElement = document.getElementById('typing-text');
+  typeWriter(typingText, typingElement, 30); // Adjust delay as needed
+});
